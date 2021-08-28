c1dbce1add398c5f0f4990254844ee3e6d9c0aab
Mthobisi-za <mthobisimtho333@gmail.com>
2021-08-28 19:32:00 +0200

    basic

---
+++ index.js
@@ -0,0 +1,32 @@
+let express = require("express");
+let app = express();
+const {Pool} = require("pg");
+var connectionString = process.env.DATABASE_URL;
+const pool = new Pool({connectionString, ssl: true})
+var exphbs = require("express-handlebars");
+const bodyParser = require("body-parser");
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.engine(
+  "handlebars",
+  exphbs({ defaultLayout: "main", layoutsDir: "views/layout" })
+);
+app.set("view engine", "handlebars");
+////used var
+const data = require("./dbLogic");
+const database = data(pool);
+app.get("/", (req,res)=>{
+  res.render("index")
+})
+app.post('/signUser', (req,res)=>{
+  var name = req.body.name;
+  var age = req.body.age;
+  var grade = req.body.grade;
+
+})
+let PORT = process.env.PORT || 3009;
+app.listen(PORT, ()=>{
+  console.log("server started on port :" + PORT)
+});
